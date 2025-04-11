7f40a093373467352406fc43b09e176b47a69d08
Vinicius Carpes de Freitas <147566186+vinicarpes@users.noreply.github.com>
2025-04-11 18:47:08 -0300

    creating the project structure with spring boot

---
+++ src/main/java/br/ifsc/edu/fln/logixpraias/LogixPraiasApplication.java
@@ -0,0 +1,13 @@
+package br.ifsc.edu.fln.logixpraias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogixPraiasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogixPraiasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ifsc/edu/fln/logixpraias/LogixPraiasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifsc.edu.fln.logixpraias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogixPraiasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
