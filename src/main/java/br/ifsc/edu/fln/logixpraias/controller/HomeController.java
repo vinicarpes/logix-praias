9566fcb54159f0623a9f654cb5a31e10f60db7d2
vinicarpes <vinicdf000@gmail.com>
2025-04-26 11:05:30 -0300

    test: created a controller to test the views

---
+++ src/main/java/br/ifsc/edu/fln/logixpraias/controller/HomeController.java
@@ -0,0 +1,22 @@
+package br.ifsc.edu.fln.logixpraias.controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        // qualquer atributo que queira passar
+        return "index";   // Retorna o template “index.html” em /templates
+    }
+
+    @GetMapping("/alerts")
+    public String alertShow(Model model) {
+        // qualquer atributo que queira passar
+        return "alert-register";   // Retorna o template “index.html” em /templates
+    }
+
+}
