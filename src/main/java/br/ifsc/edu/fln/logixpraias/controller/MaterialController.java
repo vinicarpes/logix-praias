91ffce3a1f3a4cbffef9661832964a78fa91dc10
vinicarpes <vinicdf000@gmail.com>
2025-04-26 13:39:04 -0300

    refactor: changed methods name and annotations

--- src/main/java/br/ifsc/edu/fln/logixpraias/controller/MaterialController.java
+++ src/main/java/br/ifsc/edu/fln/logixpraias/controller/MaterialController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller()
+@Controller
 @RequestMapping("/material")
 public class MaterialController {
 
-    @GetMapping("/register")
-    public ModelAndView register() {
+    @PostMapping("/register")
+    public ModelAndView create() {
         ModelAndView mv = new ModelAndView("material-register");
         return mv;
     }
