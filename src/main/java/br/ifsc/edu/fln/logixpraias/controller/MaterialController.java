183b5eb0a8cde2b6f80527717517734d9107eba3
vinicarpes <vinicdf000@gmail.com>
2025-06-24 22:53:46 -0300

    feat: method to select categoria

--- src/main/java/br/ifsc/edu/fln/logixpraias/controller/MaterialController.java
+++ src/main/java/br/ifsc/edu/fln/logixpraias/controller/MaterialController.java
@@ -1,18 +1,29 @@
 package br.ifsc.edu.fln.logixpraias.controller;
 
+import br.ifsc.edu.fln.logixpraias.model.Categoria;
+import br.ifsc.edu.fln.logixpraias.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/material")
 public class MaterialController {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     @GetMapping("/register")
-    public ModelAndView create() {
+    public ModelAndView create(Model model) {
+        List<Categoria> categorias = categoriaRepository.findAll();
+        model.addAttribute("categorias", categorias);
         ModelAndView mv = new ModelAndView("material-register");
         return mv;
     }
