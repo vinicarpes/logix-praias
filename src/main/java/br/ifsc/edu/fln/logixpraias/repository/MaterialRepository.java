616f56d90a86782523f9f9e21f79fe649da28185
vinicarpes <vinicdf000@gmail.com>
2025-06-24 22:54:39 -0300

    create: material repository

---
+++ src/main/java/br/ifsc/edu/fln/logixpraias/repository/MaterialRepository.java
@@ -0,0 +1,7 @@
+package br.ifsc.edu.fln.logixpraias.repository;
+
+import br.ifsc.edu.fln.logixpraias.model.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaterialRepository extends JpaRepository<Material, Long> {
+}
