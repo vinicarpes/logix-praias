2d9ca32a1f902ffd08febdb1745540d8a5f4e638
vinicarpes <vinicdf000@gmail.com>
2025-06-26 14:33:17 -0300

    refactor: add repository annotation

--- src/main/java/br/ifsc/edu/fln/logixpraias/repository/CategoriaRepository.java
+++ src/main/java/br/ifsc/edu/fln/logixpraias/repository/CategoriaRepository.java
@@ -2,6 +2,8 @@ package br.ifsc.edu.fln.logixpraias.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.ifsc.edu.fln.logixpraias.model.Categoria;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
--- src/main/java/br/ifsc/edu/fln/logixpraias/repository/UsuarioRepository.java
+++ src/main/java/br/ifsc/edu/fln/logixpraias/repository/UsuarioRepository.java
@@ -1,9 +1,9 @@
 package br.ifsc.edu.fln.logixpraias.repository;
 
-import br.ifsc.edu.fln.logixpraias.model.Material;
+import br.ifsc.edu.fln.logixpraias.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaterialRepository extends JpaRepository<Material, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
