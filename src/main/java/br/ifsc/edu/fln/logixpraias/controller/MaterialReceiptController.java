706acb919bf645feffd38b1f9b2d4e53aeb360e9
vinicarpes <vinicdf000@gmail.com>
2025-07-10 16:45:07 -0300

    feat: add data page countig

--- src/main/java/br/ifsc/edu/fln/logixpraias/controller/MaterialReceiptController.java
+++ src/main/java/br/ifsc/edu/fln/logixpraias/controller/MaterialReceiptController.java
@@ -1,14 +1,19 @@
 package br.ifsc.edu.fln.logixpraias.controller;
 
 import br.ifsc.edu.fln.logixpraias.model.Categoria;
+import br.ifsc.edu.fln.logixpraias.model.Estoque;
 import br.ifsc.edu.fln.logixpraias.model.Material;
 import br.ifsc.edu.fln.logixpraias.model.RecebimentoMaterial;
 import br.ifsc.edu.fln.logixpraias.repository.CategoriaRepository;
 import br.ifsc.edu.fln.logixpraias.repository.MaterialReceiptRepository;
 import br.ifsc.edu.fln.logixpraias.repository.MaterialRepository;
+import br.ifsc.edu.fln.logixpraias.repository.UsuarioRepository;
 import br.ifsc.edu.fln.logixpraias.services.EstoqueService;
 import br.ifsc.edu.fln.logixpraias.services.RecebimentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,15 +31,34 @@ public class MaterialReceiptController {
     MaterialRepository materialRepository;
     @Autowired
     RecebimentoService  recebimentoService;
+    @Autowired
+    MaterialReceiptRepository recebimentoRepository;
+    @Autowired
+    UsuarioRepository usuarioRepository;
 
     @GetMapping("")
-    public ModelAndView home() {
+    public ModelAndView home(@RequestParam(defaultValue = "1") int page,
+                             @RequestParam(defaultValue = "10") int size) {
+
         List<Categoria> categorias = categoriaRepository.findAll();
         List<Material> materiais = materialRepository.findAll();
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<RecebimentoMaterial> recebimentoPages = recebimentoRepository.findAll(pageable);
+
+        int totalPages = recebimentoPages.getTotalPages();
+        if (totalPages <= 0) {
+            totalPages = 1;
+        }
+
         ModelAndView mv = new ModelAndView("material-receipt");
+        mv.addObject("totalPages",totalPages);
+        mv.addObject("currentPage", page);
         mv.addObject("materialReceipt", new RecebimentoMaterial());
         mv.addObject("categorias", categorias);
         mv.addObject("materiais", materiais);
+        mv.addObject("recebimentos", recebimentoRepository.findAll());
+        mv.addObject("usuarios", usuarioRepository.findAll());
         return mv;
     }
 
